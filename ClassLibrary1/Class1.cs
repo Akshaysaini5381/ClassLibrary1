0cd2fa1e30213b6ee00d822438d6be6c7cdbea14
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-31 13:28:09 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void sum(int x,int y)
+        {
+            Console.WriteLine(x+y);
+        }
+        public void mul(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+    }
+}
